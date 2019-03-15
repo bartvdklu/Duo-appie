55161a9f4db50b537f9ae555d10a777663d42a32
Bart van de Klundert <b.vdklundert@outlook.com>
2019-03-15 09:03:02 +0100

    First setup

---
+++ Gamepicker/Gamepicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gamepicker
+//
+//  Created by Bart van de Klundert on 15/03/2019.
+//  Copyright © 2019 Bart van de Klundert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
