ee053f45f4cb64335b53f5bc04aa6b9f3fce33de
Nina <ninavandenbroek29@gmail.com>
2019-03-21 10:50:19 +0100

    Filter scherm toegevoegd en daarbij de knoppen door de verschillende schermen

--- Gamepicker/Gamepicker/ViewController.swift
+++ Gamepicker/Gamepicker/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var lblAantal: UILabel!
+    @IBOutlet weak var lblTijd: UILabel!
+    
+    @IBAction func sliderAantal(_ sender: UISlider) {
+        lblAantal.text = String(Int(sender.value));
+    }
+    
+    
+    @IBAction func sliderTijd(_ sender: UISlider) {
+        lblTijd.text = String(Int(sender.value));
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
