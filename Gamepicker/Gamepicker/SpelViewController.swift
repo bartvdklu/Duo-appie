ed8bea2f06cd6f2dd546dc20b689f24feb69c02f
Bart van de Klundert <b.vdklundert@outlook.com>
2019-03-28 10:56:50 +0100

    Random nummer gegenereerd imageview gemaakt naar imageslider

--- Gamepicker/Gamepicker/SpelViewController.swift
+++ Gamepicker/Gamepicker/SpelViewController.swift
@@ -15,16 +15,46 @@ class SpelViewController: UIViewController {
     @IBOutlet weak var TxtMaterialen: UITextView!
     @IBOutlet weak var TxtTijd: UITextView!
     @IBOutlet weak var lblSpel: UITextView!
+    @IBOutlet weak var ImageView: UIImageView!
     
     var ref: DatabaseReference?
     var databaseHandle:DatabaseHandle?
     
-    var GameNumber = 1;
+    var previousNumber: Int? // used in randomNumber()
+    
+    func randomNumber(number: Int) -> Int {
+        var randomNumber = Int(arc4random_uniform(UInt32(number)))
+        while previousNumber == randomNumber {
+            randomNumber = Int(arc4random_uniform(UInt32(number)))
+        }
+        previousNumber = randomNumber
+        return randomNumber
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         //set Firebase reference
         ref = Database.database().reference()
+        
+        let GameNumber = randomNumber(number: 9)
+        
+        var imagesListArray = [UIImage]()
+        
+//        for imageName in 1...3
+//        {
+//            imagesListArray.append(UIImage(named: "\(imageName).png")!)
+//        }
+        
+        // You can also use below code to add images if not want to use loop
+         imagesListArray.append(UIImage(named: "verstoppertje.png")!)
+         imagesListArray.append(UIImage(named: "Spel_beginscherm.jpg")!)
+//         imagesListArray.append(UIImage(named: "3.png")!)
+        
+        self.ImageView.animationImages = imagesListArray
+        self.ImageView.animationDuration = 10
+        self.ImageView.startAnimating()
+
         
         //retrieve content for games and listen for changes
         databaseHandle = ref?.child("Spellen").child(String(Int(GameNumber))).child("naam").observe(.value, with: { (DataSnapshot) in
